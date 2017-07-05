79d3e84cd66edb36d3f08953119b01e4b7db2bb4
Guillain Sanchez (Europe) <guillain.sanchez@dimensiondata.com>
2017-07-06 01:11:40 +0200

    App init

--- digitalException/__init__.py
+++ digitalException/__init__.py
@@ -4,8 +4,8 @@
 # Author: Guillain (guillain@gmail.com)
 # Copyright 2017 GPL - Guillain
 
-from flask import Flask, request, jsonify
-from ciscoSpark import sparkInit, sparkMsg, sparkPostMsg
+from flask import Flask, request, jsonify, render_template
+from ciscoSpark import sparkInit, sparkMsg, sparkPostMsg, sparkWebhook
 
 # Conf and create app
 app = Flask(__name__)
@@ -20,22 +20,34 @@ if( app.config['ANALYTICS'] == 'True'):
   analytics.setLevel(logging.INFO)
   analytics.addHandler(logstash.TCPLogstashHandler(app.config['ANALYTICS_HOST'], app.config['ANALYTICS_PORT'], version=app.config['ANALYTICS_VERSION']))
 
+# Initiate global room monitoring
+webhook = sparkWebhook('all')
+
 # HTTP Request -----------------------------------------------------------------------
+@app.route('/', methods=['GET'])
+def root():
+    print "hello, request is coming!"
+    return render_template('index.html')
 
 # Web the button is triggered, that's the scenario/space initialization
 @app.route('/bttn', methods=['POST'])
 def bttn():
     # Get bt.tn data & set Cisco Spark room params
     bttn = request.get_json(silent=True)
-    roomname = str(app.config['SPARK_ROOM_NAME'] + " " + bttn.get('name'))
+    try:
+        button_name = bttn.get('name')
+    except Exception as e:
+        button_name = ''
+    roomname = str(app.config['SPARK_ROOM_NAME'] + " " + button_name)
 
     # Cisco Spark: room creation, add membership
     roomid = sparkInit(roomname) 
 
-    # Initial Spark room message
+    # Send initial Spark room message
     roommsg = "New " + roomname + " created \n"
-    for k, v in bttn.items():
-        roommsg += "* " + str(k) + " : " + str(v) + "\n"
+    if button_name != '':
+        for k, v in bttn.items():
+            roommsg += "* " + str(k) + " : " + str(v) + "\n"
     welcome = sparkPostMsg(roomid,roommsg)
 
     # Post remaning action
@@ -56,7 +68,7 @@ def message():
       analytics.info(json, extra={'type': 'bot'})
 
     # Analyse the msg and perform action if necessary
-    return sparkMsg(data.get('roomId'),data.get('id'))
+    return sparkMsg(data.get('roomId'), data.get('id'))
 
 # End of App --------------------------------------------------------------------------
 if __name__ == '__main__':
